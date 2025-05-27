bdc7c7d9e0c2a055149329f32fc49a366ac8dc85
Eriicck <129214023+Eriicck@users.noreply.github.com>
2025-05-27 20:39:02 -0300

    iconos y baner fijo actualizado

--- js/main.js
+++ js/main.js
@@ -230,3 +230,18 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 }); // Fin de DOMContentLoaded
+
+
+//CAROUSEL//
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll('.carousel-image');
+  let current = 0;
+
+  function nextImage() {
+    images[current].classList.remove('active');
+    current = (current + 1) % images.length;
+    images[current].classList.add('active');
+  }
+
+  setInterval(nextImage, 4000);
+});
